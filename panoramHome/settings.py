5010acc4a6c1c7931861100e4c524c3ea8887c5e
mugiraneza <mugiranezao@gmail.com>
2023-07-05 00:57:50 +0200

    fixture

--- panoramHome/settings.py
+++ panoramHome/settings.py
@@ -70,7 +70,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_extensions',
-    'django.contrib.gis',
+    # 'django.contrib.gis',
     'rest_framework_simplejwt.token_blacklist',
     # Local app
     'ahome',
