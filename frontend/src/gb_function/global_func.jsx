884c975c5e7bb160d92d7abe1ac3ea328ea998a5
mugiraneza <mugiranezao@gmail.com>
2023-08-16 01:42:29 +0200

    replace react with svelte js

--- frontend/src/gb_function/global_func.jsx
+++
@@ -1,9 +0,0 @@
-export const  hrefUseless = (e) => {
-    e.preventDefault()
-}
-
-export const roundDecimal = (nombre, taille) => {
-    let precision = taille || 2;
-    let tmp = Math.pow(10, precision);
-    return Math.round(nombre * tmp) / tmp;
-}
